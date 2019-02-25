d9b57637930d1a472dea4be392f1ed30ee2a4cc2
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-02-25 17:20:45 +0000

    Reverse List with Bong

--- src/AmarpalAmrith/FizzBuzz/Main.java
+++ src/AmarpalAmrith/FizzBuzz/Main.java
@@ -2,6 +2,9 @@ package AmarpalAmrith.FizzBuzz;
 
 import com.sun.tools.corba.se.idl.constExpr.Not;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,6 +14,7 @@ public class Main {
             boolean Bong = false;
             boolean Bang = false;
             boolean Fezz = false;
+            boolean iReverse = false;
             if (i % 11 == 0) {
                 Bong = true;
             }
@@ -26,22 +30,39 @@ public class Main {
             if (i % 13 == 0) {
                 Fezz = true;
             }
+            if (i % 17 == 0) {
+                iReverse = true;
+            }
             StringBuilder S = new StringBuilder();
+            List <String> MyList = new ArrayList<>();
 
             if (Fizz) {
-                S.append("Fizz");
-            }
-            if (Buzz) {
-                S.append("Buzz");
-            }
-            if (Bang) {
-                S.append("Bang");
-            }
-            if (Bong) {
-                S.replace(0, S.length(), "Bong");
+                MyList.add("Fizz");
             }
             if (Fezz) {
-                S.insert(S.indexOf("B") == -1 ? 0 : S.indexOf("B"), "Fezz");
+                MyList.add("Fezz");
+            }
+            if (Buzz) {
+                MyList.add("Buzz");
+            }
+            if (Bang) {
+                MyList.add("Bang");
+            }
+            if (Bong) {
+                MyList.clear();
+                if (Fezz) {
+                    MyList.add("Fezz");
+                }
+                MyList.add("Bong");
+            }
+            if (iReverse) {
+                for (int k = MyList.size() - 1; k >= 0; k--){
+                    S.append(MyList.get(k));
+                }
+            } else {
+                for (int k = 0; k <= MyList.size() - 1; k++){
+                    S.append(MyList.get(k));
+                }
             }
             if (S.length() != 0){
                 System.out.println(S);
