f963c040e161a8e8c14cbfd01cf9f855da0c9cc9
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-02-25 11:54:50 +0000

    FizzBuzz Initial Step

--- src/AmarpalAmrith/FizzBuzz/Main.java
+++ src/AmarpalAmrith/FizzBuzz/Main.java
@@ -3,6 +3,19 @@ package AmarpalAmrith.FizzBuzz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // System.out.println(1);
+        for(int i=1; i <= 100; i++){
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0){
+                System.out.println("Buzz");
+            } else if (i % 3 == 0){
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+
+        }
+
     }
 }
