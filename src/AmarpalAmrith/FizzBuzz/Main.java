a7c745ed41b2ef2077c8a6541b6c81729afef2b0
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-02-25 12:34:58 +0000

    Multiple of 7

--- src/AmarpalAmrith/FizzBuzz/Main.java
+++ src/AmarpalAmrith/FizzBuzz/Main.java
@@ -3,14 +3,31 @@ package AmarpalAmrith.FizzBuzz;
 public class Main {
 
     public static void main(String[] args) {
-        for(int i=1; i <= 100; i++){
+        for(int i=1; i <= 105; i++){
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
+                if (i % 7 == 0){
+                    System.out.println("FizzBuzzBang");
+                } else {
+                    System.out.println("FizzBuzz");
+                }
+
             } else if (i % 5 == 0){
-                System.out.println("Buzz");
+                if (i%7 == 0){
+                    System.out.println("FizzBang");
+                }else {
+                    System.out.println("Buzz");
+                }
+
             } else if (i % 3 == 0){
-                System.out.println("Fizz");
-            } else {
+                if (i%7 == 0){
+                    System.out.println("FizzBang");
+                }else {
+                    System.out.println("Fizz");
+                }
+            } else if (i%7 ==0){
+                System.out.println("Bang");
+            }
+            else {
                 System.out.println(i);
             }
 
