f79f07c3d21319136a869a302ac49fbaeeb40cfc
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-02-25 16:33:26 +0000

    2 Stage solution with Fezz

--- src/AmarpalAmrith/FizzBuzz/Main.java
+++ src/AmarpalAmrith/FizzBuzz/Main.java
@@ -1,17 +1,56 @@
 package AmarpalAmrith.FizzBuzz;
 
+import com.sun.tools.corba.se.idl.constExpr.Not;
+
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 105; i++) {
+        for (int i = 1; i <= 300; i++) {
             boolean Fizz = false;
-            StringBuilder S = new StringBuilder();
             boolean Buzz = false;
-            boolean FizzBuzz = false;
             boolean Bong = false;
             boolean Bang = false;
             boolean Fezz = false;
-            if 
+            if (i % 11 == 0) {
+                Bong = true;
+            }
+            if (i % 3 == 0){
+                Fizz = true;
+                }
+            if (i % 7 ==0){
+                Bang = true;
+            }
+            if (i % 5 == 0){
+                Buzz = true;
+            }
+            if (i % 13 == 0) {
+                Fezz = true;
+            }
+            StringBuilder S = new StringBuilder();
+
+            if (Fizz) {
+                S.append("Fizz");
+            }
+            if (Buzz) {
+                S.append("Buzz");
+            }
+            if (Bang) {
+                S.append("Bang");
+            }
+            if (Bong) {
+                S.replace(0, S.length(), "Bong");
+            }
+            if (Fezz) {
+                S.insert(S.indexOf("B") == -1 ? 0 : S.indexOf("B"), "Fezz");
+            }
+            if (S.length() != 0){
+                System.out.println(S);
+            }
+            else if (S.length() == 0){
+                System.out.println(i);
+            }
+
         }
     }
 }
+
